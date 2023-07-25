de8e113d076945381aa60d699b7b0745b4211234
HoangTien339 <tiennh21@fpt.edu.vn>
2023-07-25 20:03:04 +0700

    Object Mapping

--- src/main/java/entities/CuaHang.java
+++ src/main/java/entities/CuaHang.java
@@ -1,11 +1,30 @@
 package entities;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "CuaHang")
 public class CuaHang {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name="Id")
+    private UUID id;
+
+    @Column(name="Ma")
     private String ma;
+
+    @Column(name="Ten")
     private String ten;
+
+    @Column(name="DiaChi")
     private String diaChi;
+
+    @Column(name="ThanhPho")
     private String thanhPho;
+
+    @Column(name="QuocGia")
     private String quocGia;
 
     public CuaHang() {
